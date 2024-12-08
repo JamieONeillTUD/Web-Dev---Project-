cff82015d18b9b50bd9b9423c6e6d74594895a4e
Jamie O'Neill <C22320301@mytudublin.ie>
2024-12-08 13:11:34 +0000

    clearing main

--- RecipeWebsite/backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Get user's dashboard (user data and their recipes)
-router.get('/dashboard', userController.getDashboard);
-
-module.exports = router;
